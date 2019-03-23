6a33753adbe96765a86e3e159da2e9e9762f5ceb
KazooKid004 <48831401+KazooKid004@users.noreply.github.com>
2019-03-22 20:43:51 -0400

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,8 @@
 #script written by KazooKid004
 # This function adds the two numbers
+
+import math
+
 def add(x, y):
    return x + y
 
@@ -23,8 +26,11 @@ print("Select operation.\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Exponent\n"
 # Take response from the user
 choice = input("Enter choice(1/2/3/4):")
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+if choice != "6":
+   num1 = float(input("Enter first number: "))
+   num2 = float(input("Enter second number: "))
+else:
+   num1 = float(input("Enter your number"))
 
 if choice == '1':
    print(num1,"+",num2,"=", add(num1,num2))
@@ -36,5 +42,7 @@ elif choice == '4':
    print(num1,"/",num2,"=", divide(num1,num2))
 elif choice == '5':
    print(num1,"**",num2,"=", exponent(num1,num2))
+elif choice == "6":
+   print(math.sqrt(num1))
 else:
    print("Invalid input")
